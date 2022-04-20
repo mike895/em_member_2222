8e3f0a7136b0b332d4fc3ab21e18a55759377a1e
root <root@ip-172-31-12-142.eu-central-1.compute.internal>
2022-04-20 06:57:56 +0000

    em_member

--- em_member/em_member/doctype/branch/test_branch.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021, 360ground and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestBranch(unittest.TestCase):
-	pass
--- em_member/em_member/doctype/member/test_member.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2021, 360ground and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestMember(unittest.TestCase):
-	pass
-
